244f543e23d2dfb634a8107ac3891d45e4e75477
Pranjalps <48259382+Pranjalps@users.noreply.github.com>
2019-03-06 02:07:22 +0530

    Add files via upload

---
+++ Scripts/PlayerManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerManager : MonoBehaviour
+{
+    #region singelton
+
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    public GameObject player;
+
+
+   /* public void GameOverPlayer(GameObject obj)
+    {
+        
+      // Destroy(obj.gameObject);
+        SceneManager.LoadScene("GameOver");
+
+    }*/
+}
